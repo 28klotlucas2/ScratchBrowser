cab9d76dbd0cbb80fde8efa6e30b988c651f3af1
ExclusiveKlotz <72287094+28klotlucas2@users.noreply.github.com>
2022-01-03 09:10:51 +0000

    Add next/previous buttons and I'm not sure if I added remixes last commit but I added a remixes page.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import urllib.request
 import json
 import logging
 import os
+import math
 
 log = logging.getLogger("werkzeug")
 log.setLevel(logging.ERROR)
@@ -15,7 +16,7 @@ app.config["SECRET_KEY"] = os.environ["secretpassword"]
 @app.before_request
 def make_session_permanent():
     if "fcb272c6-079c-4ce5-aa9c-7db84e71071b.id.repl.co" in request.url:
-      return redirect("https://scratchbrowser.28klotlucas.repl.co/")
+        return redirect("https://scratchbrowser.28klotlucas.repl.co/")
     print(request.url)
     session.permanent = True
 
@@ -43,17 +44,59 @@ def hello_world():
 @app.route("/projects/search/")
 def profilesearchmenu():
     if request.args.get("q"):
-        results = json.loads(
-            sendreq("https://api.scratch.mit.edu/search/projects?q=" +
-                    request.args.get("q").replace(" ", "%20")))
+        if request.args.get("page"):
+            results = json.loads(
+                sendreq("https://api.scratch.mit.edu/search/projects?q=" +
+                        request.args.get("q").replace(" ", "%20") +
+                        "&offset=" +
+                        str((int(request.args.get("page")) - 1) * 18)))
+        else:
+            return redirect(request.url + "&page=1")
     else:
-      return render_template("searchpage.html")
+        return render_template("searchpage.html")
     results2 = []
+    num = 0
 
     for i in results:
-        results2.append(i)
+        num = num + 1
+        if not num > 18:
+            results2.append(i)
 
-    return render_template("searchd.html", results=results2)
+    if num == 0:
+      return redirect("/projects/search?page=" + str(int(request.args.get("page")) - 1) + "&q=" + request.args.get("q"))
+
+    return render_template("searchd.html",
+                           results=results2,
+                           page=int(request.args.get("page")),
+                           search=True, q=request.args.get("q"))
+
+
+@app.route("/projects/<projid>/remixes/")
+def remixes(projid):
+    if not request.args.get("page"):
+        results = json.loads(
+            sendreq("https://api.scratch.mit.edu/projects/" + str(projid) +
+                    "/remixes/"))
+    else:
+        results = json.loads(
+            sendreq("https://api.scratch.mit.edu/projects/" + str(projid) +
+                    "/remixes?offset=" +
+                    str((int(request.args.get("page")) - 1) * 18)))
+    results2 = []
+    num = 0
+    for i in results:
+        num = num + 1
+        if not num > 18:
+            results2.append(i)
+
+    if num == 0:
+      return redirect("/projects/" + projid + "/remixes?page=" + str(int(request.args.get("page")) - 1))
+
+    return render_template("searchd.html",
+                           results=results2,
+                           search=False,
+                           page=int(request.args.get("page")),
+                           projid=projid)
 
 
 @app.route("/followers/<name>/")
@@ -123,12 +166,14 @@ def projectinfo(projid):
     stats["loves"] = str(response["stats"]["loves"])
     stats["favorites"] = str(response["stats"]["favorites"])
     stats["remixes"] = str(response["stats"]["remixes"])
-    stats["score"] = round(((((int(stats["loves"]) + int(stats["favorites"])) / 2) / int(stats["views"])) * 100) * 17.5, 2)
+    stats["score"] = round(
+        ((((int(stats["loves"]) + int(stats["favorites"])) / 2) /
+          int(stats["views"])) * 100) * 17.5, 2)
 
     if stats["score"] > 100:
-      stats["score"] = "100"
+        stats["score"] = "100"
     else:
-      stats["score"] = str(stats["score"])
+        stats["score"] = str(stats["score"])
 
     response["tags"] = []
 
@@ -170,12 +215,33 @@ def check_under_maintenance():
 
 @app.route("/google08ced3cd04c4329e.html")
 def e():
-  return render_template("google08ced3cd04c4329e.html")
+    return render_template("google08ced3cd04c4329e.html")
 
 
 @app.errorhandler(503)
 def error503(e):
     return "I'm doing koder stuff wait till I dun is."
+
+
+@app.errorhandler(404)
+def error404(e):
+    if request.url.startswith(
+            "http://scratchbrowser.28klotlucas.repl.co/projects/"
+    ) or request.url.startswith(
+            "https://scratchbrowser.28klotlucas.repl.co/projects/"):
+        return "Sorry, that project is either private or it dosen't exist."
+    return """<html class="js-focus-visible" data-js-focus-visible=""><head><title>404 Not Found</title>
+</head><body><h1>Not Found</h1>
+<p>The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.</p>
+</body></html>"""
+
+
+@app.errorhandler(500)
+def error500(e):
+    return """<html class="js-focus-visible" data-js-focus-visible=""><head><title>404 Not Found</title>
+</head><body><h1>Not Found</h1>
+<p>The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.</p>
+</body></html>"""
 
 
 app.run(host='0.0.0.0', port=8080)
